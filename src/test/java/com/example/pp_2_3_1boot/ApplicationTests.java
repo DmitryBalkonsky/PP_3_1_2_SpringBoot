c91e11bf4f2d119dad4dfa151f1578dab27dfae9
DmitryBalkonsky <dima.balynin2@gmail.com>
2023-01-30 16:59:19 +0300

    Initial commit

---
+++ src/test/java/com/example/pp_2_3_1boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pp_2_3_1boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
